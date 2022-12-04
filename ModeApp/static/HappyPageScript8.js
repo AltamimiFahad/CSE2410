8c1febaf547e9355af183b1d073e6c17a982846e
AltamimiFahad <114172764+AltamimiFahad@users.noreply.github.com>
2022-12-03 22:11:10 -0500

    .

---
+++ ModeApp/static/HappyPageScript8.js
@@ -0,0 +1,36 @@
+let btn = document.querySelector('#bn');
+
+function getRandomNumber(min, max) {
+    let step1 = max - min + 1;
+    let step2 = Math.random() * step1;
+    return Math.floor(step2) + min;
+}
+
+function createArrayOfNumbers(start, end){
+    let myArray = [];
+    for(let i = start; i <= end; i++) {
+        myArray.push(i);
+    }
+    return myArray;
+}
+
+let numbersArray = createArrayOfNumbers(0, Id.length-1);
+btn.addEventListener("click", () => {
+    if(numbersArray.length === 0){
+        alert("There is no more phrases");
+        return;
+    }
+
+    let randomIndex = getRandomNumber(0, numbersArray.length-1);
+    let randomNumber = numbersArray[randomIndex];
+    numbersArray.splice(randomIndex, 1)
+    
+    // let authorPic = '<img src="'+ AuthorPic[randomNumber] +'" style="border-radius: 90%" alt=""/>';
+    let authorPic = '<img src="'+ AuthorPic[randomNumber] +'" style="border-radius: 50%; width: 250px;" alt=""/>';
+
+
+    document.getElementById("a").innerHTML =(Author[randomNumber])
+    document.getElementById("p").innerHTML =(Phrase[randomNumber])
+    document.getElementById("pic").innerHTML =(authorPic)
+
+ });
---
+++ ModeApp/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import *
+
+
+
+def index(request):
+    template = loader.get_template('ModeAppPage.html')
+    return HttpResponse(template.render())
+
+def happy(request):
+    template = loader.get_template('HappyPage.html')
+    HappyDatabase = happy_database.objects.all().values_list('id', 'author', "phrase", "authorPic")
+    context = {
+        'myDataBase': HappyDatabase,
+    }
+    return HttpResponse(template.render(context))
+
+def sad(request):
+    return HttpResponse("Hello, world. You're at the sad page.")
+
+def angry(request):
+    return HttpResponse("Hello, world. You're at the angry page.")
+
+def nervous(request):
+    return HttpResponse("Hello, world. You're at the nervous page.")
+
+
+
+
